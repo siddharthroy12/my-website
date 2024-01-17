85a8d11fcfab121b3653f0def57ee71f0f5402d1
Siddharth Roy <siddharthroy36912@gmail.com>
2024-01-17 19:45:49 +0530

    Initialized NextJS and shadcn/ui

--- src/supabaseClient.ts
+++
@@ -1,9 +0,0 @@
-import { createClient } from "@supabase/supabase-js";
-
-// Create a single supabase client for interacting with your database
-const supabase = createClient(
-  import.meta.env.SUPABASE_URL,
-  import.meta.env.SUPABASE_ANON_KEY,
-);
-
-export default supabase;
