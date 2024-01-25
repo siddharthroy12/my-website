d413493e34c55d2849080265e813600ac908d2b7
Siddharth Roy <siddharthroy36912@gmail.com>
2024-01-25 22:09:39 +0530

    made markdown pretty

--- app/posts/[slug]/page.tsx
+++ app/posts/[slug]/page.tsx
@@ -3,6 +3,9 @@ import Markdown from "react-markdown";
 import remarkGfm from "remark-gfm";
 import "./markdown.scss";
 import { Card } from "@/components/ui/card";
+import { Prism as SyntaxHighlighter } from "react-syntax-highlighter";
+import { duotoneSea } from "react-syntax-highlighter/dist/cjs/styles/prism";
+import rehypeRaw from "rehype-raw";
 
 async function getPostMarkdown(slug: string) {
   const blog = await supabase
@@ -25,7 +28,7 @@ export default async function Post({ params }: { params: { slug: string } }) {
   const date = new Date(res.created_at);
 
   return (
-    <div className="p-10">
+    <div className="p-10" suppressHydrationWarning>
       <h1 className="mt-10 pb-2 text-3xl font-semibold tracking-tight transition-colors first:mt-0">
         {res.title}
       </h1>
@@ -38,10 +41,41 @@ export default async function Post({ params }: { params: { slug: string } }) {
       <Markdown
         className="markdown"
         remarkPlugins={[remarkGfm]}
+        rehypePlugins={[rehypeRaw]}
         components={{
-          pre: ({ children }) => {
+          code(props) {
+            const { children, className, node, ...rest } = props;
+            const match = /language-(\w+)/.exec(className || "");
+            let language = "markdown";
+            if (match) {
+              language = match[1];
+            }
             return (
-              <Card className="overflow-auto w-full">
+              // @ts-ignore
+              <SyntaxHighlighter
+                {...rest}
+                PreTag="div"
+                children={String(children).replace(/\n$/, "")}
+                language={language}
+                style={duotoneSea}
+                useInlineStyles
+                customStyle={{
+                  fontFamily: "monospace",
+                  background: "none",
+                  padding: 0,
+                  margin: 0,
+                  border: "none",
+                  textShadow: "none",
+                  boxShadow: "none",
+                  overflow: "unset",
+                }}
+                codeTagProps={{ style: { background: "none" } }}
+              />
+            );
+          },
+          pre({ children }) {
+            return (
+              <Card className="overflow-auto p-5 rounded-lg bg-zinc-950 dark:bg-zinc-900 w-full">
                 <pre>{children}</pre>
               </Card>
             );
