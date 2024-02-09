6f6b8833878337848604aa39ee9adfb64fdf4269
Siddharth Roy <siddharthroy36912@gmail.com>
2024-02-09 14:25:30 +0530

    Fixed size

--- app/posts/layout.tsx
+++ app/posts/layout.tsx
@@ -27,7 +27,7 @@ export default async function PostsLayout({
     <div className="flex md:h-screen h-[calc(100vh-48px)]">
       <Blogs blogs={blogs} />
       <Separator orientation="vertical" />
-      <div className="overflow-auto">{children}</div>
+      <div className="overflow-auto w-full">{children}</div>
     </div>
   );
 }
