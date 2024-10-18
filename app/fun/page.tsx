830a9ac63e5b1f4f5d9fe46bf647824c7c1c0f17
Siddharth Roy <siddharthroy36912@gmail.com>
2024-10-18 14:09:17 +0530

    Added a game

--- app/fun/page.tsx
+++ app/fun/page.tsx
@@ -1,5 +1,7 @@
 export default function Fun() {
   return (
-    <div className="w-full h-full grid place-content-center">Cooming soon</div>
+    <div className="w-full h-full grid px-8 py-8">
+      <iframe frameborder="0" src="https://itch.io/embed-upload/5778463?color=ff2a4b" allowfullscreen="yes" width="300" height="300"><a href="https://siddharthroy12.itch.io/rockets">Play Rockets! on itch.io</a></iframe>
+    </div>
   );
 }
